ccd3da7f9b4a0ab13228cbf0e227cd6ff9f9a8c4
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-17 12:03:47 +0200

    Create basic User entity

---
+++ src/main/java/com/maciejbihun/models/User.java
@@ -0,0 +1,31 @@
+package com.maciejbihun.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "USER_SEQ")
+    @SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ", allocationSize = 1)
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(name = "NAME", updatable = true, length = 30)
+    private String name;
+
+    @Basic(optional = false)
+    @Column(name = "SURNAME", updatable = true, length = 30)
+    private String surname;
+
+    @Basic(optional = false)
+    @Column(name = "LOGIN", updatable = true, length = 30)
+    private String login;
+
+    @Basic(optional = false)
+    @Column(name = "PASSWORD", updatable = true, length = 30)
+    private String password;
+
+}
