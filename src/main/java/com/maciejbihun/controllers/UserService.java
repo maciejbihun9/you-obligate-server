da94c3f581d7149dd11e73a458ddbcd8536085c8
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-18 13:00:04 +0200

    Update UserService methods to new UserRepository implementation

--- src/main/java/com/maciejbihun/controllers/UserService.java
+++ src/main/java/com/maciejbihun/controllers/UserService.java
@@ -3,7 +3,10 @@ package com.maciejbihun.controllers;
 import com.maciejbihun.models.User;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface UserService {
     ResponseEntity<User> getUser(Long id);
+    ResponseEntity<List<User>> getAllUsers();
     ResponseEntity<User> saveUser(User user);
 }
--- src/main/java/com/maciejbihun/controllers/impl/UserServiceImpl.java
+++ src/main/java/com/maciejbihun/controllers/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Transactional
@@ -26,13 +27,20 @@ public class UserServiceImpl implements UserService {
     @Override
     @RequestMapping(value="/users/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(userRepository.getUser(id), HttpStatus.OK);
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.map(user -> new ResponseEntity<>(user, HttpStatus.FOUND)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value="/users", method = RequestMethod.POST)
     public ResponseEntity<User> saveUser(@RequestBody User user) {
-        user = userRepository.saveUser(user);
+        user = userRepository.save(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
