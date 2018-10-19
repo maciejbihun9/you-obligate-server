7a0f0661ef13bdd951388fc9c12d7cbc2e1c797c
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-19 11:21:33 +0200

    Update createUserAccount method

--- src/main/java/com/maciejbihun/controller/UserService.java
+++ src/main/java/com/maciejbihun/controller/UserService.java
@@ -9,5 +9,4 @@ public interface UserService {
     ResponseEntity<User> getUser(Long id);
     ResponseEntity<List<User>> getAllUsers();
     ResponseEntity<User> createUserAccount(User user);
-    createUserAccount
 }
