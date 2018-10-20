de3d761aa78284c1fbf4dceaf43f21c3a99c9ec9
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-20 21:00:07 +0200

    Add method to generate csrf tokens

--- src/main/java/com/maciejbihun/controller/HomeController.java
+++ src/main/java/com/maciejbihun/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.maciejbihun.controller;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -11,6 +17,13 @@ public class HomeController {
     public String home(Model model) {
         // return "forward:/index.html";
         return "forward:/index.html";
+    }
+
+    @RequestMapping(value="/csrf-token", method= RequestMethod.GET)
+    public @ResponseBody
+    String getCsrfToken(HttpServletRequest request) {
+        CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+        return token.getToken();
     }
 
 }
