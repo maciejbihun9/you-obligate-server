2564415cb99bcad3d36737995d8d71e3d675a48b
Maciej Bihun <maciej.bihun@hpe.com>
2018-10-20 22:22:13 +0200

    [YOK-27] - Add user field setter and getter to UserRegisteredService

--- src/main/java/com/maciejbihun/models/UserRegisteredService.java
+++ src/main/java/com/maciejbihun/models/UserRegisteredService.java
@@ -47,6 +47,14 @@ public class UserRegisteredService {
         return id;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public LocalDateTime getCreatedDateTime() {
         return createdDateTime;
     }
