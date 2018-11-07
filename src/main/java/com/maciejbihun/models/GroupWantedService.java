d23df8f1438abe8cbe65b9283e43a58326d2e747
Maciej Bihun <maciej.bihun@hpe.com>
2018-11-07 11:25:51 +0100

    Add GroupWantedService

---
+++ src/main/java/com/maciejbihun/models/GroupWantedService.java
@@ -0,0 +1,9 @@
+package com.maciejbihun.models;
+
+import java.util.List;
+
+public class GroupWantedService {
+    private int experienceInYears;
+    private List<String> serviceBaseWords;
+    private String serviceDescription;
+}
