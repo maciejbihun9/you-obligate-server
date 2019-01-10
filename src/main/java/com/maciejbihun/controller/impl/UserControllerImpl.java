9a279de826613d5db17a2704ee656f0fef43229e
Maciej Bihun <maciej.bihun@hpe.com>
2019-01-10 12:45:35 +0100

    Make user rest controller basic url as users

--- src/main/java/com/maciejbihun/controller/impl/UserControllerImpl.java
+++ src/main/java/com/maciejbihun/controller/impl/UserControllerImpl.java
@@ -21,7 +21,7 @@ import java.util.List;
 /**
  * @author Maciej Bihun
  */
-@RestController
+@RestController(value = "/users")
 public class UserControllerImpl implements UserController {
 
     @Autowired
