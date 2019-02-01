dbf37fdd269cba1b85cad460cfeff2aa07dcbd68
Bihun <XBBNWYJ@eur.bnymellon.net>
2019-02-01 16:24:29 +0100

    Add test for checking users ability to access getting group join requests for specific obligation group

--- src/test/java/com/maciejbihun/service/GroupJoinRequestServiceTest.java
+++ src/test/java/com/maciejbihun/service/GroupJoinRequestServiceTest.java
@@ -1,7 +1,39 @@
 package com.maciejbihun.service;
 
+import com.maciejbihun.Application;
+import com.maciejbihun.HibernateConf;
+import com.maciejbihun.exceptions.NotEnoughPermissionsException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
- * Created by XBBNWYJ on 2/1/2019.
+ * @author Maciej Bihun
  */
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@SpringBootTest(classes = {Application.class, HibernateConf.class})
+@ActiveProfiles("test")
 public class GroupJoinRequestServiceTest {
+
+    @Autowired
+    GroupJoinRequestService groupJoinRequestService;
+
+    @Test(expected = AccessDeniedException.class)
+    @WithMockUser(username="maciek1", roles={"USER"})
+    public void getGroupJoinRequestsByObligationGroupIdMethodCallIsAllowedByAdmin(){
+        int obligationGroupId = 1;
+        try {
+            groupJoinRequestService.getGroupJoinRequestsByObligationGroupId(obligationGroupId);
+        } catch (NotEnoughPermissionsException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
